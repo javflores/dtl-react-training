eb1fc8e7bac5fea61c88426f467d5fdb9c552e83
javflores <javicaria@ingenieros.com>
2016-10-26 18:07:27 +0100

    Adding babel and webpack setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import {render} from 'react-dom';
+//
+// class App extends React.Component {
+//     render () {
+//         return (
+//             <div>React with hot reloading is the cats pajamas!</div>
+//         )
+//     }
+// }
+//
+// render(<App></App>, document.getElementById('app'));
+console.log("Hello");
